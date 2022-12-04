1c8a46c668a4dcc4b90c22724d5a80d5832ee9f8
None <53135862+baelow@users.noreply.github.com>
2022-12-04 14:28:44 +0800

    Update handle_main.py

--- handle_main.py
+++ handle_main.py
@@ -10,8 +10,8 @@ s = sys.argv[5]
 print(datetime.datetime.now())
 print('当前设定申请的格林尼治时间为：%s'%t)
 print(f"设定的北京时间为：{datetime.datetime.strftime(datetime.datetime.strptime(t,'%H:%M:%S')+datetime.timedelta(hours=8),'%H:%M:%S')}\n\n-----START----\n\n")
-my_ymq = DDDDOCR_School_BNU_YMQ(d,k,p,t,s)
-my_ymq.main_apply_task()
+# my_ymq = DDDDOCR_School_BNU_YMQ(d,k,p,t,s)
+# my_ymq.main_apply_task()
 
 
 
